011b932b2fb7a17f411f2e62078b6dcd2e61fb9a
ZakyAkmal <76078293+ZakyAkmal@users.noreply.github.com>
2020-12-16 07:26:27 +0700

    Add files via upload

---
+++ data/simpanglima_1.js
@@ -0,0 +1,1 @@
+var json_simpanglima_1 = {"type":"FeatureCollection","name":"simpanglima_1","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"id":"1"},"geometry":{"type":"Point","coordinates":[110.409869263498294,-6.983693139584922]}}]}
